36a1aa3a23e2758990841ed37166ffa4ecb15150
dheivasri <murugandheivasri@gmail.com>
2023-10-09 23:31:58 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { FormsModule } from '@angular/forms';
+import { PopupComponent } from './popup/popup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    PopupComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
